26995c87f2d0662b94b1fa515eb308e5b1929657
YaoKundi <kundiyao@gmail.com>
2021-06-22 17:18:14 -0400

    add inner-project clone detection code

---
+++ src/clone_detection/clone_detection.py
@@ -0,0 +1,170 @@
+"""
+Clone detection in selected java projects
+"""
+import multiprocessing
+import os
+import logging
+import src.util.utils as utils
+import pandas as pd
+import shutil
+import tarfile
+import glob
+import subprocess
+
+logger = logging.getLogger(__name__)
+
+class CloneDetection:
+    def __init__(self, language, granularity, clonetype):
+        self.language = language
+        self.granularity = granularity
+        self.clonetype = clonetype
+        self.NiCadRoot = utils.getPath('NICAD_ROOT')
+        # The temp folder for tar file to decompress and anlayze
+        self.tmp = '../../temp/projects'
+        utils.create_folder_if_not_exist(self.tmp)
+        # Result archive path
+        self.res_dir = '../../result/clone_detection'
+        utils.create_folder_if_not_exist(self.res_dir)
+
+    def clone_detection_in_project(self, df):
+        """
+        Perform clone detection with NiCad 6.2
+        Returns
+        -------
+        df: The dataframe with projects to be analyzed
+        """
+        for i, row in df.iterrows():
+            repo_path = row['repo_path']
+            repo_id = row['project_id']
+
+            if not os.path.isfile(repo_path):
+                logger.error('Unable to find path: {}'.format(repo_path))
+                continue
+
+            # The project will be decompressed under this directory, and NiCad results will be written here as well
+            tmp_out_dir = os.path.abspath(os.path.join(self.tmp, str(repo_id)))
+
+            # Clean temp project if it exists. This could happen when a previous job collapsed
+            if os.path.isdir(tmp_out_dir):
+                shutil.rmtree(tmp_out_dir)
+
+            # Decompress tar to temp folder
+            tar = tarfile.open(repo_path, "r:gz")
+            tar.extractall(path=tmp_out_dir)
+            tar.close()
+
+            # The temporary decompressed project directory
+            tmp_out_proj_dir = os.path.join(tmp_out_dir, os.listdir(tmp_out_dir)[0])
+
+            # NiCad clone deteciton
+            # Example: ./nicad5 functions java systems/JHotDraw54b1 default-report
+            cmd = ' '.join([
+                './nicad6',
+                self.granularity,
+                self.language,
+                tmp_out_proj_dir,
+                self.clonetype
+            ])
+            p = subprocess.Popen(cmd, shell=True, cwd=self.NiCadRoot)
+            try:
+                p.communicate()
+            except Exception as e:
+                logger.error('Clone detection fail at project {}, {}'.format(row['repo_name'], str(e)))
+                shutil.rmtree(tmp_out_dir)
+                continue
+            # Check if process succeed
+            if p.returncode != 0:
+                logger.error('Error in running clone detection for project {}. Command: {}"'.format(
+                    row['repo_name'], cmd
+                ))
+                shutil.rmtree(tmp_out_dir)
+                continue
+
+            # Move result to location
+            nicad_output_list = glob.glob(tmp_out_proj_dir + '_{}*'.format(self.granularity))
+            # Save all results into a tar file
+            res_tar_f = os.path.join(self.res_dir, '_'.join([str(repo_id), os.path.basename(repo_path)]))
+
+            with tarfile.open(res_tar_f, mode='w:gz') as tar:
+                for f_nicad_out in nicad_output_list:
+                    tar.add(f_nicad_out, arcname=os.path.basename(f_nicad_out))
+            logger.info('Clone detection finished. Results are saved in {}'.format(res_tar_f))
+            # Remove temp out folder
+            shutil.rmtree(tmp_out_dir)
+
+
+def logging_setup(args):
+    """
+    Prepare logging,
+    Parameters
+    ----------
+    size_types
+
+    Returns
+    -------
+
+    """
+    size_types = [x.strip() for x in args.size_level.split(',')]
+    size_types_str = '_'.join(size_types)
+
+    # out_f = os.path.abspath('../../result/proj_clone/{}.csv'.format(
+    #     '_'.join([*args.language, args.granularity, args.clonetype, size_types_str])
+    # ))
+    # utils.output_prepare(out_f)
+
+    # Set logger
+    utils.setlogger(f_log=os.path.abspath('../../log/clone_detection/{}.log'.format(
+        '_'.join([args.language, args.granularity, args.clonetype, size_types_str]),
+        level=logging.INFO
+    )))
+
+def load_projects_list(args):
+    """
+    Choose all projects to be analyzed and merge them into the same dataframe
+    Parameters
+    ----------
+    args
+
+    Returns
+    -------
+
+    """
+    sloc_dir = '../../result/proj_sloc'
+    dfs = []
+
+    # Concatenate all size types to be analyzed
+    for size_type in args.size_level.split(','):
+        df_projects = utils.csv_loader(os.path.join(sloc_dir, 'filesize_sloc_{}.csv'.format(size_type.strip())))
+        dfs.append(df_projects)
+    return pd.concat(dfs)
+
+def parallel_run(df, func):
+    """
+    Run function in parallel
+    :param df:
+    :param chunks:
+    :return:
+    """
+    jobs = []
+    for df_sp in utils.chunkify(data=df, chunks=utils.getWorkers()):
+        jobs.append(
+            multiprocessing.Process(target=func, args=(df_sp, ))
+        )
+    [j.start() for j in jobs]
+    [j.join() for j in jobs]
+
+if __name__ == '__main__':
+    args, _ = utils.parse_args_clone_detection()
+    # Compressed files directory
+    cdetec = CloneDetection(
+        language=args.language,
+        granularity=args.granularity,
+        clonetype=args.clonetype
+    )
+    # Prepare logging
+    logging_setup(args)
+    # Load target df
+    df = load_projects_list(args)
+
+    cdetec.clone_detection_in_project(df)
+    #parallel_run(df=df, func=cdetec.clone_detection_in_project)
--- src/util/utils.py
+++ src/util/utils.py
@@ -4,7 +4,7 @@ import platform
 import socket
 import pandas as pd
 import numpy as np
-
+import logging
 from functools import wraps
 from threading import Thread
 from multiprocessing import Process, Pool
@@ -66,6 +66,51 @@ def parse_args_size_level(*args, **kwargs):
 
     return parser.parse_known_args()
 
+def parse_args_clone_detection(*args, **kwargs):
+    parser = argparse.ArgumentParser(description='Input args for calculating the size/SLOC of a project', *args, **kwargs)
+    parser.add_argument('-l',
+                        '--size_level',
+                        type=str,
+                        default=None,
+                        help="""
+                        Please input the size levels to be analyzed
+                        The types of levels: 
+                            - small
+                            - medium
+                            - large
+                            - vlarge
+                        You can specify on level or multiple levels joined by comma
+                        """,
+                        required=True)
+    parser.add_argument('--language',
+                        type=str,
+                        default='java',
+                        help="The language type to be analyzed. \n"
+                             "Currently, NiCad uses the included plugin to handle source files written in the "
+                             "following languages: C (.c), C# (.cs), Java (.java), Python (.py), "
+                             "PHP (.php), Ruby (. Rb), WSDL (.wsdl) and ATL (.atl).")
+    parser.add_argument('--granularity',
+                        type=str,
+                        default='blocks',
+                        help="Currently NiCad can handle granularity: functions and blocks.")
+    parser.add_argument('--clonetype',
+                        type=str,
+                        default='default',
+                        help="The clone types to be examined. \n"
+                             "NiCad can detect 1-3 types of clones.\n"
+                             "The predefined configuration for each NiCad configuration is inconfig/ In the subdirectory.\n"
+                             "The predefined configurations include type1, type2, type2c, type3-1, type3-2, and type3-c\n"
+                             "To detect type 1 (exact) clones, setthreshold=0.0\n"
+                             "To detect type 2 (rename) clones, setthreshold=0.0 with rename=blind\n"
+                             "To detect type 2c (consistent rename) clones, setthreshold = 0.0 with rename=consistent\n"
+                             "To detect type 3-1 (near miss exact) clones, setthreshold=0.3\n"
+                             "To detect type 3-2 (near miss rename) clones, setthreshold = 0.3 with rename=blind\n"
+                             "To detect type 3-2c (near miss and consistently rename) clones, setthreshold=0.3 with rename=consistent\n"
+                             "Note1: type 2 includes type 1, type 3-1 includes type 1, and type 3-2 includes types 1 and 2.\n"
+                             "Note2: default uses type 3-2")
+    return parser.parse_known_args()
+
+
 def getPath(param_str: str):
     """
     Get path according to current OS platform/System Name
@@ -86,6 +131,12 @@ def getPath(param_str: str):
                 'BRAIN2': '/home/local/SAIL/kundi/BACKUP/dataset/GitJavaLoggingRepos',
                 'PINKY': '/home/local/SAIL/kundi/BACKUP/dataset/GitJavaLoggingRepos',
                 'COMPUTECANADA': ''
+            }
+        },
+        'NICAD_ROOT':{
+            'DARWIN': '/Users/yaokundi/Documents/Project/2021/ExternalTools/NiCad-6.2',
+            'LINUX': {
+                'BRAIN2': '/home/local/SAIL/kundi/configs/NiCad-6.2'
             }
         }
     }
@@ -126,7 +177,7 @@ def getWorkers(cpus=None):
             raise TypeError('Unrecogzied input type %s' % str(cpus.__name__))
     limit_usage_machines = ['brain2', 'pinky']
     if socket.gethostname().lower() in limit_usage_machines:
-        return cpu_count // 2
+        return cpu_count // 4
     else:
         # Use all cpus if not on SAIL servers
         return cpu_count
@@ -184,3 +235,39 @@ def chunkify(data, chunks):
     elif isinstance(data, pd.DataFrame):
         df_split = np.array_split(data, chunks)
         return df_split
+
+
+def output_prepare(f):
+    if not os.path.isdir(os.path.dirname(f)):
+        os.makedirs(os.path.dirname(f))
+    if os.path.isfile(f):
+        os.remove(os.path.abspath(f))
+
+
+def setlogger(f_log, level=logging.INFO):
+    if not os.path.isdir(os.path.dirname(f_log)):
+        os.makedirs(os.path.dirname(f_log))
+    logging.basicConfig(
+        filename=os.path.join(f_log),
+        level=level,
+        format='%(asctime)s - %(process)d - %(levelname)s - %(message)s',
+        datefmt='%d-%b-%y %H:%M:%S')
+
+def csv_loader(f):
+    if not os.path.isfile(f):
+        raise FileNotFoundError('File {} not found'.format(f))
+    return pd.read_csv(f)
+
+def create_folder_if_not_exist(d):
+    """
+    Create directory if not exists
+    Parameters
+    ----------
+    d
+
+    Returns
+    -------
+
+    """
+    if not os.path.isdir(d):
+        os.makedirs(d)
